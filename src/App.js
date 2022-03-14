20b183deb0e4812c4e3016352c9500925fc40413
Chirag Aparadh <69748682+git-chirag@users.noreply.github.com>
2022-03-14 13:23:00 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { TextField } from "@mui/material";
+import { useState } from "react";
+import CustomizedHook from "./Select";
+
+// const terminals = new Array(26).fill(0).map((_, index) => ({
+//     title: String.fromCharCode(65 + index),
+// }));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [terminals, setTerminals] = useState([]);
+    const [nonTerminals, setNotTerminals] = useState([]);
+    const [productions, setProductions] = useState([]);
+
+    const handleSumit = (e) => {
+        e.preventDefault();
+    };
+
+    const handleTerminalChange = (e, value) => {
+        console.log(value);
+    };
+
+    const handleNonTerminalChange = (e, value) => {
+        console.log(value);
+    };
+
+    const nonTerminalsArray = new Array(26).fill(0).map((_, index) => ({
+        title: String.fromCharCode(65 + index),
+    }));
+
+    const terminalsArray = new Array(26).fill(0).map((_, index) => ({
+        title: String.fromCharCode(97 + index),
+    }));
+
+    return (
+        <>
+            <form onSubmit={handleSumit}>
+                <h2>Enter non terminals</h2>
+                <CustomizedHook
+                    optionsArray={nonTerminalsArray}
+                    label={"Non Terminals"}
+                    currentDefaultValue={18}
+                    handleChange={handleNonTerminalChange}
+                />
+                <h2>Enter terminals</h2>
+                <CustomizedHook
+                    optionsArray={terminalsArray}
+                    label={"Terminals"}
+                    currentDefaultValue={null}
+                    handleChange={handleTerminalChange}
+                />
+                <TextField />
+            </form>
+        </>
+    );
 }
 
 export default App;
